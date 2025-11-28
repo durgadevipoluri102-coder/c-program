d7341af89185b4916cf8a3c903a5dd51892689c8
durgadevipoluri102-coder <durgadevi.poluri102@gmail.com>
2025-11-28 11:55:28 +0530

    Add files via upload

---
+++ call by reference.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+ void swap(int* , int*);
+ int main ()
+{ ;
+    int a,b;
+	printf("Enter a and b values:");
+	scanf("%d%d",&a,&b);
+	printf("\n main() Before  swap a=%d,b=%d",a,b);
+	swap(&a,&b);
+	printf("\n main()after swap a=%d,b=%d",a,b);
+}
+void swap(int *a,int *b)
+{
+	int temp=*a;
+	*a=*b;
+	*b=temp;
+	printf("\n Inside swap() function a=%d,b=%d",*a,*b);
+	
+}
